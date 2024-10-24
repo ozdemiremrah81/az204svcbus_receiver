acdceada25620411be500e111fa05d06c6700775
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-24 16:19:26 +0300

    processor code lines has been added

--- Program.cs
+++ Program.cs
@@ -1,44 +1,54 @@
 ﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
 // connection string to your Service Bus namespace
 string connectionString = Environment.GetEnvironmentVariable("connectionString");
 
-// name of your Service Bus topic
-string queueName = "az204-queue";
-// the client that owns the connection and can be used to create senders and receivers
-ServiceBusClient client;
-
-// the sender used to publish messages to the queue
-ServiceBusSender sender;
-
-// Create the clients that we'll use for sending and processing messages.
+ServiceBusProcessor processor;
 client = new ServiceBusClient(connectionString);
-sender = client.CreateSender(queueName);
 
-// create a batch 
-using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
-
-for (int i = 1; i <= 3; i++)
-{
-    // try adding a message to the batch
-    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
-    {
-        // if an exception occurs
-        throw new Exception($"Exception {i} has occurred.");
-    }
-}
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
 
 try
 {
-    // Use the producer client to send the batch of messages to the Service Bus queue
-    await sender.SendMessagesAsync(messageBatch);
-    Console.WriteLine($"A batch of three messages has been published to the queue.");
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    await Task.Delay(TimeSpan.FromMinutes(1));
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
 }
 finally
 {
     // Calling DisposeAsync on client types is required to ensure that network
     // resources and other unmanaged objects are properly cleaned up.
-    await sender.DisposeAsync();
+    await processor.DisposeAsync();
     await client.DisposeAsync();
 }
 
-Console.WriteLine("Follow the directions in the exercise to review the results in the Azure portal.");
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // complete the message. messages is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
